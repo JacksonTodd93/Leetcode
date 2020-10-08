929aec9d954e27047e89c844521b040871c67d5e
Jackson Todd <jacksonttodd@gmail.com>
2020-10-08 16:58:11 -0400

    Added mergeIntervals

---
+++ mergeIntervals.js
@@ -0,0 +1,47 @@
+/**
+ * https://leetcode.com/problems/merge-intervals/
+ *
+ *
+ *
+ * Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+For any given entry in intervals, it is guaranteed that intervals[i][0] <= intervals[i][1]
+ */
+
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const mergeHelper = function (arr) {
+    let mergeCount = 0;
+    for (i = 1; i < arr.length; i += 1) {
+      if (arr[i][0] <= arr[i - 1][1]) {
+        arr[i][0] = Math.min(arr[i - 1][0], arr[i][0]);
+        arr[i][1] = Math.max(arr[i - 1][1], arr[i][1]);
+        arr.splice(i - 1, 1);
+        mergeCount++;
+      }
+    }
+    if (mergeCount === 0) {
+      return arr;
+    } else {
+      return mergeHelper(arr);
+    }
+  }
+  return mergeHelper(intervals);
+};
+
+/**
+ * Accepted answer.
+ * Beats 42% of JavaScript submissions for speed,
+ * Beats 25% of JavaScript submissions for memory
+ */
